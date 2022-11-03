1e8f278f54c5085470f0147f40bd921f0289e6bf
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-11-03 16:39:20 +0800

    added Cineplex class

--- Classes/Cineplex.java
+++ Classes/Cineplex.java
@@ -1,7 +1,28 @@
+// Done by Mingyang
 import java.util.ArrayList;
 
 public class Cineplex{
     private String name;
     private int cineplexID;
+    private ArrayList<Cinema> cinemas = new ArrayList<>();
 
+    // Default cinema constructor: should never be called
+    public Cineplex() {
+        this.cineplexID = -1;
+        this.name = "Cinema";
+    }
+
+    // Constructor
+    public Cineplex(String name, int cineplexID) {
+        this.cineplexID = cineplexID;
+        this.name = name;
+    }
+
+    public int getCineplexID() {
+        return cineplexID;
+    }
+
+    public String getCineplexName() {
+        return name;
+    }
 }
--- Classes/Vendor.java
+++ Classes/Vendor.java
@@ -3,15 +3,19 @@ import java.util.ArrayList;
 
 public class Vendor {
     private String vendorName;
+    public ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>();
 
     // Default vendor name: Cathay
     public Vendor() {
         this.vendorName = "Cathay Cineplexes";
-        ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>();
     }
 
     public String getVendorName() {
         return vendorName;
+    }
+
+    public void addCineplex(String cineplexName) {
+        cineplexes.add(new Cineplex(cineplexName, cineplexes.size() + 1));
     }
 }
 
