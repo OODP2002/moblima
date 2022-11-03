11be525c82610126edf5ca4e08400ec200e05007
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-11-03 17:42:53 +0800

    added CinemaClass feature

--- Classes/CinemaClass.java
+++ Classes/CinemaClass.java
@@ -1,3 +1,16 @@
 // Done by Mingyang
 public class CinemaClass {
+    private CinemaClassLevels cinemaClass;
+
+    public CinemaClass() {
+        this.cinemaClass = CinemaClassLevels.STANDARD;
+    }
+
+    public CinemaClass(CinemaClassLevels cinemaClass) {
+        this.cinemaClass = cinemaClass;
+    }
+
+    public CinemaClassLevels getCinemaClass() {
+        return cinemaClass;
+    }
 }
