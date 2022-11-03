669b88e01f030f2de005eb9d9a9efd64dfd62c94
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-11-03 15:22:02 +0800

    added Vendor class

--- Classes/Cineplex.java
+++ Classes/Cineplex.java
@@ -2,28 +2,7 @@ import java.util.ArrayList;
 
 public class Cineplex{
     private String name;
-    private int id;
-    private ArrayList<Cinema> cinema;
-    private int numCinema;
+    private int cineplexID;
 
-    public Cineplex(String name, int id, ArrayList<Cinema> cinema){
-        this.name = name;
-        this.id = id;
-        this.cinema=cinema;
-        this.numCinema=cinema.size();
-    }
-
-    
-    public ArrayList<Cinema> getCinema(){
-        return cinema;
-    }
-
-    public void setCinema(int i, Cinema cinema){
-        return cinema.set(i, cinema);
-    }
-
-    public int getNumCinema(){
-        return this.numCinema;
-    }
-
+    public
 }
---
+++ Classes/Vendor.java
@@ -0,0 +1,18 @@
+// Done by Mingyang
+import java.util.ArrayList;
+
+public class Vendor {
+    private String vendorName;
+
+    public Vendor() {
+        ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>;
+    }
+
+    public void getVendorName() {
+        return vendorName;
+    }
+
+    public String setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+}
