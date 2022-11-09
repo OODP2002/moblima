14f57ddb0e5ef69be1fde7ba571b7822d7dacd70
lowzk <lowzhekai69@gmail.com>
2022-11-09 10:52:47 +0800

    feat/printAllMovies

--- Classes/demo.java
+++ Classes/demo.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class demo {
     public static void main(String[] args) {
         MovieStore movieStore = MovieStore.getInstance();
-        Movie temp = movieStore.getMovie(0);
-        temp.printInfo();
+        movieStore.printAllMovies();
     }
 }
