c8d0811674815bd3de6bca64aa37e5f4482fbda2
Trigon25 <53526750+Trigon25@users.noreply.github.com>
2022-11-09 15:42:08 +0800

    update to main

--- Classes/MoviePersonnelInterface.java
+++
@@ -1,7 +0,0 @@
-// Done by Marc
-import java.util.ArrayList;
-
-public interface MoviePersonnelInterface extends PersonInterface{
-    public abstract ArrayList<Movie> getMovies();
-    public abstract void addToMovies(Movie cMovie);
-}
