9bf2467c7f052814348695b086a5ea7278370369
lowzk <lowzhekai69@gmail.com>
2022-11-09 15:57:59 +0800

    feat/listbytop5 function
    0 for list by sales
    1 for list by rating

--- Classes/demo.java
+++ Classes/demo.java
@@ -3,12 +3,6 @@ import java.util.ArrayList;
 public class demo {
     public static void main(String[] args) {
         MovieStore movieStore = MovieStore.getInstance();
-        System.out.println(movieStore.getMovie(0).getAgeRating().getDetailString());
-        Movie temp = new Movie("Avengers", 3, new MovieDuration(123), new ShowingStatus(Status.COMINGSOON), new Synopsis("Temporary synopsis"), new ViewingMode(View._3D), new MovieHype(Hype.BLOCKBUSTER), new MovieSales(33), new AgeRating(AgeEnum.NC16));
-        temp.addMoviePersonnel("James Bond", Role.DIRECTOR);
-        temp.addMoviePersonnel("Marc", Role.CAST);
-        temp.getOverallReviews().addReview(4, "Pretty decent movie");
-        movieStore.addMovie(temp);
-        movieStore.writeToMoviesFile();
+        movieStore.ListTop5(0);
     }
 }
