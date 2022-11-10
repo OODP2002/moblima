8efc9eec97ad3debc2f62b5f36bfe336f3901c4f
Trigon25 <53526750+Trigon25@users.noreply.github.com>
2022-11-10 20:01:20 +0800

    Changed MovieGoer

--- Classes/MovieGoerDemo.java
+++ Classes/MovieGoerDemo.java
@@ -2,10 +2,6 @@ public class MovieGoerDemo {
     public static void main(String[] args){
         MovieGoer person = new MovieGoer();
         person.listAllMovies();
-        person.listBy();
-        person.listBy();
-        // person.searchMovie();
-        // person.searchMovie(69);
         person.buyTicket();
     }
 }
