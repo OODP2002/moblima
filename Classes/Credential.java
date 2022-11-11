19c26bddfa2278f6716420ef9118c4afe064bd11
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-11-11 19:33:29 +0800

    fixing txt files

--- Classes/Credential.java
+++ Classes/Credential.java
@@ -2,28 +2,28 @@ public class Credential {
     //Attributes
     private String username;
     private String password;
-    private AdminRole role; 
+    private AdminRole role;
 
     //Constructor 
-    public Credential(String username, String password, AdminRole role){
+    public Credential(String username, String password, AdminRole role) {
         this.username = username;
         this.password = password;
         this.role = role;
     }
 
     //Operations
-    public String getUsername(){
+    public String getUsername() {
         return this.username;
     }
 
-    public AdminRole getRole(){
+    public AdminRole getRole() {
         return this.role;
     }
 
     //Change username 
-    public boolean setUsername(String newUsername, String password){
+    public boolean setUsername(String newUsername, String password) {
         //User needs to provide the corrrect password to change username
-        if(this.password.equals(password)){
+        if (this.password.equals(password)) {
             this.username = newUsername;
             return true;
         } else {
@@ -32,10 +32,10 @@ public class Credential {
     }
 
     //Change password
-    public boolean setPassword(String oldPassword, String newPassword){
+    public boolean setPassword(String oldPassword, String newPassword) {
         //User needs to provide the correct old password to change password
         //To do: implement password checks to ensure that password is 8 char, alphanumeric, contains capital letters and symbols
-        if(this.password.equals(oldPassword)){
+        if (this.password.equals(oldPassword)) {
             this.password = newPassword;
             return true;
         } else {
@@ -44,24 +44,14 @@ public class Credential {
     }
 
     //Check if correct password
-    public boolean check(String password){
-        
+    public boolean check(String password) {
+
         return this.password.equals(password);
     }
 
     public String getPassword() {
         return password;
     }
-
-    //Converts Credential instance to a string
-    public String toString(){
-        String roleStr = "";
-        switch(this.role){
-            case CINEMASTAFF:
-                roleStr = "CinemaStaff";
-                break;
-        }
-        return this.username + "|" + this.password + "|" + roleStr + "|";
-    }
 }
+
 
