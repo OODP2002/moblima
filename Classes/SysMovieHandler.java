0d8240ad8ef7a39d8dc17af17564f7c69fcc467c
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2022-11-12 11:54:12 +0800

    wip remove movies module

--- Classes/SysMovieHandler.java
+++ Classes/SysMovieHandler.java
@@ -14,6 +14,18 @@ public interface SysMovieHandler {
 
     default void removeMovie() {
         MovieStore movieStore = MovieStore.getInstance();
+
+        System.out.println("=".repeat(29));
+        System.out.println("\tREMOVE MOVIE MODULE\t");
+        System.out.println("=".repeat(29));
+
+        // Display all movie name, movie status and showing status
+        for (Movie movie: movieStore.getMovieHashMap().values()) {
+            System.out.println("-".repeat(29));
+            System.out.println(movie.getMovieName());
+            System.out.printf("Movie ID: %s\t Status: %s\n", movie.getMovieID(), movie.getShowingStatus().toString());
+        }
+
         System.out.print("Enter movie ID you wish to remove");
         String movieID = sc.nextLine();
         if (movieStore.removeMovie(movieID) == null)
